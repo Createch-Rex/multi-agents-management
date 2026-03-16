4d31eeb7fe0c47f95fc13761f45c68b765d498e7
Qoostech-Rex <developer@qoostech.com>
2026-03-16 15:26:55 +0800

    create worker check auth wrapper

--- utils/manage_utils.py
+++ utils/manage_utils.py
@@ -1,6 +1,6 @@
+from database.models import User, Worker
 from fastapi import Request, Depends
 from sqlalchemy.orm import Session
-from database.models import User
 from typing import Optional
 from functools import wraps
 from utils import common
@@ -34,6 +34,31 @@ def auth_required(func):
     async def wrapper(request: Request, db: Session = Depends(common.get_db), *args, **kwargs):
         user = get_system_user_from_header(request, db)
         if not user:
+            return common.standard_response(status="expire")
+        kwargs['db'] = db
+        kwargs['request'] = request
+        response = await func(*args, **kwargs)
+        db.commit()
+        return response
+    return wrapper
+
+
+def get_worker_from_header(request: Request, db: Session) -> Optional[Worker]:
+    token = request.headers.get('X-Worker-Token')
+    if not token:
+        return None
+
+    worker = db.query(Worker).filter(Worker.token == token).first()
+    if not worker:
+        return None
+    return worker
+
+
+def worker_auth_required(func):
+    @wraps(func)
+    async def wrapper(request: Request, db: Session = Depends(common.get_db), *args, **kwargs):
+        worker = get_worker_from_header(request, db)
+        if not worker:
             return common.standard_response(status="expire")
         kwargs['db'] = db
         kwargs['request'] = request

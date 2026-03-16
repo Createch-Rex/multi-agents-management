2a07c8bff150c1d9acef98782f9e1031cd1eb094
Qoostech-Rex <developer@qoostech.com>
2026-03-16 15:06:48 +0800

    add worker router

--- api/router.py
+++ api/router.py
@@ -1,7 +1,9 @@
 from api.frontend.router import frontend_router
+from api.worker.router import worker_router
 from fastapi import APIRouter
 
 
 api_router = APIRouter()
 
 api_router.include_router(frontend_router, prefix="/frontend")
+api_router.include_router(worker_router, prefix="/worker")
---
+++ api/worker/router.py
@@ -0,0 +1,4 @@
+from fastapi import APIRouter
+
+
+worker_router = APIRouter()

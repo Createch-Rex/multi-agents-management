f3dd8b5399f52310f7ca488d240466f9a6e670e3
Qoostech-Rex <developer@qoostech.com>
2026-03-18 20:19:59 +0800

    update migrate to add default admin user

--- migrate.py
+++ migrate.py
@@ -1,7 +1,17 @@
 from database.database import engine, Base, get_db
 from database.models import *
-import json
 
 if __name__ == "__main__":
     for i in Base.__subclasses__():
         i.metadata.create_all(bind=engine)
+
+    for db in get_db():
+        user = User()
+        user.create()
+        user.username = 'admin'
+        user.password = User.generate_password('admin123')
+        user.role = 'admin'
+        user.email = 'bob@minion.com'
+        db.add(user)
+        db.commit()
+
